be8e97a0dcd4ea7b5c2b9cb524f1899116363488
VijayAkilan <154575563+VijayAkilan@users.noreply.github.com>
2024-05-13 13:20:13 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -5,7 +5,7 @@ region="ap-south-1"
 resource "aws_instance" "AWSEC2"{
 ami = "ami-001843b876406202a"
 instance_type = "t2.micro"
-key_name= "devopsdecbatchmss"
+key_name= "demoserver"
 security_groups = ["launch-wizard-2"]
 tags = {
 Name = "terraformserver"
